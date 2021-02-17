5f6b8046e959390bdab5ae5e62e70338671c5784
Łukasz Woronkiewicz <woronkiewicz@gmail.com>
2021-02-17 22:07:44 +0100

    feat: add abstract dataset class

--- color_generator/datasets/dataset.py
+++ color_generator/datasets/dataset.py
@@ -1,28 +1,26 @@
 """Dataset class to be extended by datasets-specific classes."""
+from torch.utils.data import Dataset
 from pathlib import Path
 import argparse
-import os
 
 
-class Dataset:
+class DefaultDataset(Dataset):
     """Simple abstract class for datasets."""
 
     @classmethod
     def data_dirname(cls):
-        return Path(__file__).resolve().parents[4] / "data"
+        return Path(__file__).resolve().parents[2] / "data"
 
-
-    def load_or_generate_data(self):
+    def load_and_generate_data(self):
         pass
-
-
-def _download_raw_dataset(metadata):
-    pass
 
 
 def _parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--subsample_fraction", type=float, default=None, help="If given, is used as the fraction of data to expose.",
+        "--subsample_fraction",
+        type=float,
+        default=None,
+        help="If given, is used as the fraction of data to expose.",
     )
     return parser.parse_args()
