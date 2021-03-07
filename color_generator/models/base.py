261d2f282d98c8e5d1c776bf96f9e42ff8346e26
Łukasz Woronkiewicz <woronkiewicz@gmail.com>
2021-03-07 23:51:09 +0100

    feat: add fit method to base model

--- color_generator/models/base.py
+++ color_generator/models/base.py
@@ -1,71 +1,115 @@
 """Model class, to be extended by specific types of models."""
-# pylint: disable=missing-function-docstring
 from pathlib import Path
-from typing import Callable, Dict, Optional
-
-from tensorflow.keras.models import Model as KerasModel
-from tensorflow.keras.optimizers import Adam
-
-
-DIRNAME = Path(__file__).parents[1].resolve() / "weights"
+import torch.optim as optim
+import torch.nn as nn
+import torch
 
 
 class Model:
     """Base class, to be subclassed by predictors for specific type of data."""
 
     def __init__(
-        self,
-        dataset_cls: type,
-        network_fn: Callable[..., KerasModel],
-        dataset_args: Dict = None,
-        network_args: Dict = None,
+        self, dataloader, network_fn, network_args=None, dataset_args=None, device="cpu"
     ):
-        self.name = f"{self.__class__.__name__}_{dataset_cls.__name__}_{network_fn.__name__}"
 
-        if dataset_args is None:
-            dataset_args = {}
-        self.data = dataset_cls(**dataset_args)
+        self.name = ""
+        self.dataset = None
+        self.dataset_args = dataset_args
+        self._dataloader = dataloader
+        self.device = device
 
         if network_args is None:
             network_args = {}
-        self.network = network_fn(**network_args)
-        self.network.summary()
+        self.network = network_fn(**network_args).to(self.device)
 
     @property
-    def weights_filename(self) -> str:
-        DIRNAME.mkdir(parents=True, exist_ok=True)
-        return str(DIRNAME / f"{self.name}_weights.h5")
+    def weights_filename(self):
+        p = Path(__file__).resolve().parents[2] / "weights"
+        p.mkdir(parents=True, exist_ok=True)
+        return str(p / f"{self.name}_weights.pt")
 
-    def fit(
-        self, dataset, batch_size: int = 32, epochs: int = 10, callbacks: list = None,
-    ):
-        if callbacks is None:
-            callbacks = []
+    def __set_name_and_dataloaders(self, dataset, test=False):
+        if not self.dataset:
+            if self.dataset_args is None:
+                self.dataset_args = {}
+            self.dataset = dataset(**self.dataset_args)
 
-        self.network.compile(loss=self.loss(), optimizer=self.optimizer())
+        if test:
+            _, _, self._test_loader = self._dataloader(self.dataset)
+            return None
 
-        self.network.fit(
-            dataset,
-            epochs=epochs,
-            callbacks=callbacks,
-            # validation_data=test_sequence,
-            use_multiprocessing=False,
-            workers=1,
-            shuffle=True,
-        )
+        self.name = f"{self.__class__.__name__}_{dataset.__name__}_{self.network.__class__.__name__}"
+        self._train_loader, self._valid_loader, _ = self._dataloader(self.dataset)
 
-    def loss(self):
-        # pylint: disable=no-self-use
-        return "mse"
+    def fit(self, dataset, epochs=10):
 
-    def optimizer(self):  # pylint: disable=no-self-use
-        return Adam()
+        self.__set_name_and_dataloaders(dataset)
 
-    def load_weights(self):
-        self.network.load_weights(self.weights_filename)
+        for epoch in range(epochs):
+            self.network.train()
+            running_loss = 0.0
+            for i, batch in enumerate(self.train_loader):
+                # forward and backward propagation
+                input_ids = batch["input_ids"].to(self.device)
+                attention_mask = batch["attention_mask"].to(self.device)
+                targets = batch["target"].to(self.device)
+                outputs = self.network(input_ids, attention_mask)
+                loss = self.criterion(outputs, targets)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+                # save results
+                running_loss += loss.item()
+                if i > 0 and i % 100 == 0:
+                    stats = (
+                        f"Epoch: {epoch+1}/{epochs}, batch: {i}/{len(self._train_loader)}, "
+                        f"train_loss: {running_loss/i:.5f}"
+                    )
+                    print("\\r" + stats, end="", flush=True)
+                    with open("stats.log", "a") as f:
+                        print(stats, file=f)
+
+            # calculate loss and accuracy on validation dataset
+            with torch.no_grad():
+                val_loss = self.evaluate()
+            stats = f"Epoch: {epoch+1}/{epochs}, train_loss: {running_loss/i:.5f}, valid_loss: {val_loss:.5f}"
+            print("\\r" + stats)
+            with open("stats.log", "a") as f:
+                print(stats, file=f)
+
+            # save after each epoch
+            self.save_weights()
+
+            # check for early stopping
+            self.early_stopping(val_loss, self.network)
+            if self.early_stopping.early_stop:
+                print("Early stopping.")
+                break
+
+        self.load_weights(early_stopping=True)
+        self.save_weights()
+        print("\nFinished training")
+
+    def criterion(self):
+        return nn.MSELoss(reduction="mean").to(self.device)
+
+    def optimizer(self):
+        return optim.AdamW(self.network.parameters())
+
+    def load_weights(self, early_stopping=False):
+        if early_stopping:
+            f = "early_stopping_checkpoint.pt"
+        else:
+            self.weights_filename
+        self.network.load_state_dict(torch.load(f))
 
     def save_weights(self):
-        self.network.save_weights(self.weights_filename)
+        torch.save(self.network.state_dict(), self.weights_filename)
 
     def evaluate(self, dataset):
-        return self.network.evaluate(dataset)
+        pass
+
+    def early_stopping(self):
+        pass
+
