272533ed13a231e8dc59d838b3a0c628b01692ee
thormic <michal.thor5@gmail.com>
2021-03-11 14:16:02 +0100

    Refactor missing description handling

--- color_generator/api/app.py
+++ color_generator/api/app.py
@@ -3,6 +3,7 @@
 import os, random
 from flask import Flask, request, jsonify, render_template
 from color_generator.models.color_model import ColorModel
+from color_generator.color_predictor import ColorPredictor
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
@@ -14,13 +15,6 @@ example_colors = ["Ferrari Red",
                   "Kowalski's Hair Color"]
 
 
-def plot_rgb(rgb):
-    # data = [[rgb]]
-    plt.figure(figsize=(2,2))
-    plt.imshow(rgb, interpolation='nearest')
-    plt.show()
-
-
 @app.route("/")
 def index():
     """Provide simple health check route."""
@@ -29,11 +23,10 @@ def index():
 
 @app.route("/show_color", methods=["GET", "POST"])
 def send_color():
-    # handle the POST request
     if request.method == 'POST':
-        print ("POST")
         color_desc = request.form.get('color_desc')
         color = predict(color_desc)
+        print ("POST", color_desc, color)
         return render_template("submitted.html", color=color)
     print ("GET")
     color = "rgb(255, 0, 0)"
@@ -42,13 +35,10 @@ def send_color():
 
 
 def predict(color_desc):
-    if color_desc is None:
-        print("Color description was not valid.")
-        return None
-    input_text = color_desc
-    predictor = ColorModel()
-    predictor.load_weights()
-    pred = predictor.predict_on_text(input_text.lower())
+    input_text = color_desc.lower()
+    predictor = ColorPredictor()
+    pred = predictor.predict(input_text)
+    print(input_text, pred)
     pred = [int(x*255) for x in pred[0]]
     color = f"rgb({pred[0]}, {pred[1]}, {pred[2]})"
     return color
