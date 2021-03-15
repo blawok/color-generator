761f2af5f8980db8691aaff135666fbd0d6160b6
Łukasz Woronkiewicz <woronkiewicz@gmail.com>
2021-03-16 00:23:40 +0100

    feat: add test for dataloader

--- tests/test_ColorsDataset.py
+++ tests/test_ColorsDataset.py
@@ -1,14 +1,18 @@
+import pytest
 from torch import Tensor
 from color_generator.datasets import ColorsDataset
 
 
-def test_dataset_len():
-    dataset = ColorsDataset()
+@pytest.fixture
+def dataset():
+    return ColorsDataset()
+
+
+def test_dataset_len(dataset):
     assert len(dataset) > 0
 
 
-def test_dataset_structure():
-    dataset = ColorsDataset()
+def test_dataset_structure(dataset):
     test_item = dataset[0]
     assert len(test_item) == 3
     assert isinstance(test_item["input_ids"], Tensor)
---
+++ tests/test_Dataloaders.py
@@ -0,0 +1,38 @@
+import pytest
+from torch import Tensor
+from color_generator.datasets import ColorsDataset, DataLoaders
+
+
+@pytest.fixture
+def dataloader():
+    return DataLoaders(ColorsDataset())
+
+
+def test_train_loader(dataloader):
+    batch = next(iter(dataloader.train_loader))
+    assert len(batch["input_ids"].shape) == 2
+    assert len(batch["attention_mask"].shape) == 2
+    assert len(batch["target"].shape) == 2
+    assert isinstance(batch["input_ids"], Tensor)
+    assert isinstance(batch["attention_mask"], Tensor)
+    assert isinstance(batch["target"], Tensor)
+
+
+def test_valid_loader(dataloader):
+    batch = next(iter(dataloader.valid_loader))
+    assert len(batch["input_ids"].shape) == 2
+    assert len(batch["attention_mask"].shape) == 2
+    assert len(batch["target"].shape) == 2
+    assert isinstance(batch["input_ids"], Tensor)
+    assert isinstance(batch["attention_mask"], Tensor)
+    assert isinstance(batch["target"], Tensor)
+
+
+def test_test_loader(dataloader):
+    batch = next(iter(dataloader.test_loader))
+    assert len(batch["input_ids"].shape) == 2
+    assert len(batch["attention_mask"].shape) == 2
+    assert len(batch["target"].shape) == 2
+    assert isinstance(batch["input_ids"], Tensor)
+    assert isinstance(batch["attention_mask"], Tensor)
+    assert isinstance(batch["target"], Tensor)
