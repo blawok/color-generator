49721654aa81fce0c178e66eac3d75df73742b1b
Łukasz Woronkiewicz <woronkiewicz@gmail.com>
2021-03-19 15:59:10 +0100

    feat: allow to initiate ColorPredictor

--- tasks/predict_color.sh
+++ tasks/predict_color.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 3 ]; then
     echo "Expecting: "
     echo "1) path to training config file,"
-    echo "2) a colorname (in double quotes if more than one word),"
-    echo "3) flag --no_plot (optional),"
-    echo "4) flag --gpu (optional)."
+    echo "2) path to file with model weights,"
+    echo "3) a colorname (in double quotes if more than one word),"
+    echo "4) flag --no_plot (optional),"
+    echo "5) flag --gpu (optional)."
     exit 1
 fi
 
 
 export PYTHONPATH=$(pwd)
-pipenv run python color_generator/color_predictor.py $1 $2 $3 $4
+pipenv run python color_generator/color_predictor.py $1 $2 $3 $4 $5
 
 
